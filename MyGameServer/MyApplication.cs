fef31e17daac4b600d7d3fdd939d4b4acffe98f4
ChiLi_Lin <904981663@163.com>
2017-05-31 17:41:35 +0800

    The Server Of PhotonTest

---
+++ MyGameServer/MyApplication.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Photon.SocketServer;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net;
+using System.IO;
+using log4net.Config;
+
+namespace MyGameServer
+{
+    public class MyApplication : ApplicationBase
+    {
+
+        private static readonly ILogger log = ExitGames.Logging.LogManager.GetCurrentClassLogger();
+
+        public static void Log(string message)
+        {
+            log.Info(message.ToString());
+        }
+
+        //创建连接
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new MyClientPeer(initRequest);
+        }
+
+        //服务器启动时
+        protected override void Setup()
+        {
+            InitLogging();
+
+            Log("It Fucking Setup!");
+        }
+
+        protected virtual void InitLogging()
+        {
+            ExitGames.Logging.LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+            GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");
+            GlobalContext.Properties["LogFileName"] = "-666-" + this.ApplicationName;
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(this.BinaryPath, "log4net.config")));
+        }
+
+        protected override void TearDown()
+        {
+            Log("It Fucking TearDown!");
+        }
+    }
+}
---
+++ MyGameServer/MyClientPeer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace MyGameServer
+{
+    public class MyClientPeer : ClientPeer
+    {
+        public MyClientPeer(InitRequest initRequest) : base(initRequest)
+        {
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+        }
+    }
+}
